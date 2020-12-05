9e1bda234b214aa35a4cdd7608e079db1a2234d6
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2020-12-06 09:13:24 +1030

    Update newMusic.php

--- Step 4 - Spotify Clone - Create Your Own Music Player-Recorder/newMusic.php
+++ Step 4 - Spotify Clone - Create Your Own Music Player-Recorder/newMusic.php
@@ -36,31 +36,6 @@ include("includes/includedFiles.php");
     </div>
 </div>
 
-
-<?php
-// include("./includes/config.php");
-
-
-if(isset($_POST['playlistId']) && isset($_POST['songId'])) {
-	$playlistId = $_POST['playlistId'];
-	$songId = $_POST['songId'];
-
-	$orderIdQuery = mysqli_query($con, "SELECT IFNULL(MAX(playlistOrder) + 1, 1) as playlistOrder FROM playlistSongs WHERE playlistId='$playlistId'");
-	$row = mysqli_fetch_array($orderIdQuery);
-	$order = $row['playlistOrder'];
-
-	$query = mysqli_query($con, "INSERT INTO playlistSongs VALUES(NULL, '$songId', '$playlistId', '$order')");
-
-}
-else {
-	echo "PlaylistId or songId was not passed into addToPlaylist.php";
-}
-
-?>
-
-<!--  -->
-        
-
           <div id="box">
             <div id="star1"></div>
             <div id="sun" title="Sun *Star*"></div>
